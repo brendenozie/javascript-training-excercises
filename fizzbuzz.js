04e311cbcdb2383ee52c59e02ca404d10533c564
brendenozie <brendenodhiambo@gmail.com>
2017-03-02 17:07:55 +0300

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,2 @@
+for(var counter=1; counter<=100;counter++){if(counter%3==0 && counter%5==0){console.log(counter+" fizzBuzz")}else if(counter%5==0){console.log(counter+" buzz")}else if(counter%3==0){console.log(counter+" fizz")}}
+                                           
--- for_loop.js
+++ for_loop.js
@@ -1,12 +1,1 @@
-var str="#";
-for (var counter=0;counter<=7;counter++){
-  if (counter==0){
-    console.log(str)
-  }
-  else if(str.length==7){
-    break;
-  }
-  else{
-    str+="#";console.log(str);
-  }
-}
+var str="#"; for (var counter=0;counter<=7;counter++){if (counter==0){console.log(str)}else if(str.length==7){break;}else{str+="#";console.log(str);}}

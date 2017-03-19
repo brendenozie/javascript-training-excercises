1d34d3de245c4e2c698fb2e8af5e431d4ca557e4
brendenozie <brendenodhiambo@gmail.com>
2017-03-19 17:05:16 +0300

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,2 +1,9 @@
-for(var counter=1; counter<=100;counter++){if(counter%3==0 && counter%5==0){console.log(counter+" fizzBuzz")}else if(counter%5==0){console.log(counter+" buzz")}else if(counter%3==0){console.log(counter+" fizz")}}
+for(var counter=1; counter<=100;counter++){
+  if(counter%3==0 && counter%5==0){
+    console.log(counter+" fizzBuzz")
+  }else if(counter%5==0){
+    console.log(counter+" buzz")}
+  else if(counter%3==0){console.log(counter+" fizz")
+                       }
+}
                                            
